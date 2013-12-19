8acfbe986716700fe182ec709b2a3a99ca13851e
mcFlying.marty@gmail.com <mcFlying.marty@gmail.com@de6290fe-def4-3e1c-600e-8081bd68225f>
2013-12-19 14:22:52 +0000

    pre-version avant héritage

--- trunk/prototype/lightdark/src/com/me/lightdark/modeles/Case.java
+++ trunk/prototype/lightdark/src/com/me/lightdark/modeles/Case.java
@@ -8,14 +8,26 @@ public class Case {
 	
 	Vector2 position = new Vector2();
 	Rectangle cadre = new Rectangle();
+	
+	interface type_case{} 
+	
+	enum type_case_generique implements type_case {
+		TERRE, EAU, VEGETATION, MONTAGNE, CAVERNE
+	}
+	
 
+	boolean bloquante;
+	boolean ombre;
+	
 	public Case(Vector2 position) {
 		this.position = position;
 		this.cadre.setX(position.x);
 		this.cadre.setY(position.y);
 		this.cadre.height = TAILLE;
 		this.cadre.width = TAILLE;
-
+		
+		this.bloquante = false;
+		this.ombre = false;
 	}
 	
 	// **** GETTERS ****
@@ -27,4 +39,27 @@ public class Case {
 		return cadre;
 	}
 	
+	/*
+	 *  Les deux attributs sont ombre et bloquante
+	 */
+	public void setBloquant(boolean b){
+		this.bloquante = b;
+	}
+	
+	public boolean getBloquant(){
+		return this.bloquante;
+	}
+	
+	public void setOmbre(boolean b){
+		this.ombre = b;
+	}
+	
+	public boolean getOmbre(){
+		return this.ombre;
+	}
+	/*
+	public ? getType(){
+		return "";
+	}
+	*/
 }
