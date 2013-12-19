8c3c54f9b97c27e7dc114d568dc80a3d7b22e9c9
mcFlying.marty@gmail.com <mcFlying.marty@gmail.com@de6290fe-def4-3e1c-600e-8081bd68225f>
2013-12-19 23:36:00 +0000

    correction bug sur la collision des projectiles

--- trunk/prototype/lightdark/src/com/me/lightdark/modeles/Niveau.java
+++ trunk/prototype/lightdark/src/com/me/lightdark/modeles/Niveau.java
@@ -10,6 +10,8 @@ public class Niveau {
 	private int hauteur;
 	private Case[][] cases;
 	private Rectangle[][] bloquantes;
+	private Array<Monstre> monstres;
+	
 	public Niveau() {
 		// TODO Auto-generated constructor stub
 		cases = new Case[11][11];
@@ -43,10 +45,13 @@ public class Niveau {
 		}
 	}
 	
+	public Array<Monstre> getMonstres(){
+		return this.monstres;
+	}
 	public void createCaseWithShadow (int i, int j) {
 		cases[i][j] = new Case(new Vector2(i,j));
 		bloquantes[i][j] = cases[i][j].getCadre();
-
+		monstres = new Array<Monstre>();
 	}
 	
 	
@@ -57,7 +62,7 @@ public class Niveau {
 		//cases[0][10] = new Case(new Vector2(0,10));
 		//cases[10][0] = new Case(new Vector2(10,0));
 		
-		cases[1][1] = new Case(new Vector2(1,1));
+		/*cases[1][1] = new Case(new Vector2(1,1));
 		cases[2][1] = new Case(new Vector2(2,1));
 		(cases[2][1]).setFriction(0.1f);
 		
@@ -67,6 +72,9 @@ public class Niveau {
 		bloquantes[1][1] = cases[1][1].getCadre();
 		bloquantes[9][9] = cases[9][9].getCadre();
 		bloquantes[5][5] = cases[5][5].getCadre();
+		
+		monstres.add(new Monstre(new Vector2(5,1)));
+		
 		/*
 		cases.add(new Case(new Vector2(0,10)));
 		cases.add(new Case(new Vector2(10,0)));
@@ -74,6 +82,25 @@ public class Niveau {
 
 		cases.add(new Case(new Vector2(0,0)));
 		*/
+		for (int i=0; i<11 ; i++){
+			for (int j = 0; j<11;j++){
+				
+				if (i == 0 || i == 10 || j == 0 || j == 10){
+					cases[i][j] = new Case(new Vector2(i,j));
+					bloquantes[i][j] = cases[i][j].getCadre();
+				}
+			}
+		}
+		
+		for (int i=0; i<11 ; i++){
+			if (i%2 == 0){
+				for(int j = 1; j <9 ; j++){
+					cases[i][j] = new Case(new Vector2(i,j));
+					bloquantes[i][j] = cases[i][j].getCadre();
+				}
+			}
+		}
+		
 	}
 	
 	private void demo2(){
