135ca36cad0b986c5f0596bc1ab95412d2f06598
mickael.forgh@gmail.com <mickael.forgh@gmail.com@de6290fe-def4-3e1c-600e-8081bd68225f>
2013-12-22 13:39:32 +0000

    - Ajout de la version repensée du code

---
+++ trunk/prototype/lightdark/src/com/me/lightdark/modeles/Dark.java
@@ -0,0 +1,6 @@
+package com.me.lightdark.modeles;
+
+public enum Dark {
+	IDLE, SHADOWWALKING, GRABBING, DYING, TAMING 
+} //Différenciation entre MARCHANt et SHADOWWALKING : la ShadowForm ne peut marche QUE sur des ombres!
+
